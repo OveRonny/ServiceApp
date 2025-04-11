907106ecc35daaff91baa348e5b67cf9e58f39d9
Ove Ronny Bolme <overonny@progorb.no>
2025-04-11 09:24:41 +0200

    Added getVehicleById in vehicles

---
+++ serviceApp.Server/Features/Vehicles/GetvehicleById.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using serviceApp.Server.Abstractions;
+using serviceApp.Server.Abstractions.RequestHandling;
+using serviceApp.Server.Data;
+
+namespace serviceApp.Server.Features.Vehicles;
+
+public static class GetvehicleById
+{
+    public record class Query(int Id) : IQuery<Response>;
+    public record Response(int Id, string Make, string Model, string Year, string Color, string LicensePlate, DateTime DateCreated);
+    public class Handler(ApplicationDbContext context) : IQueryHandler<Query, Response>
+    {
+        private readonly ApplicationDbContext context = context;
+        public async Task<Result<Response>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var vehicle = await context.Vehicles.FindAsync(request.Id);
+            if (vehicle == null)
+            {
+                return Result.Fail<Response>($"Vehicle with ID {request.Id} not found.");
+            }
+            return new Response(vehicle.Id, vehicle.Make, vehicle.Model, vehicle.Year, vehicle.Color, vehicle.LicensePlate, vehicle.DateCreated);
+        }
+    }
+}
+
+[ApiController]
+[Route("api/vehicle")]
+public class GetVehicleByIdController(ISender sender) : ControllerBase
+{
+    private readonly ISender sender = sender;
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<GetvehicleById.Response>> GetVehicleById(int id)
+    {
+        var result = await sender.Send(new GetvehicleById.Query(id));
+        if (result.Failure)
+        {
+            return NotFound(result.Error);
+        }
+        return Ok(result);
+    }
+}
+
+
